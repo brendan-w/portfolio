9e3ff47ce2a304e6f68356188ae610808a65032e
Brendan Whitfield <brendanw@windworksdesign.com>
2015-10-09 15:46:03 -0400

    added temporary routes for uncomitted graphics

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ var express = require("express");
 var server = express();
 
 server.use(express.static(path.join(__dirname, "/site")));
-server.get("/*", function(req, res) {
+
+server.get("/:project", function(req, res) {
 	res.sendFile(path.join(__dirname, "/site/index.html"));
 });
+
+
+//temporary, until I start finalize & commit the graphics
+server.get("/assets/grid.png", function(req, res) {
+	res.sendFile(path.join(__dirname, "../portfolio/grid.png"));
+});
+
+server.get("/assets/laser.jpg", function(req, res) {
+	res.sendFile(path.join(__dirname, "../portfolio/laser.jpg"));
+});
+
 
 console.log("Listening on 8080");
 server.listen(8080);
