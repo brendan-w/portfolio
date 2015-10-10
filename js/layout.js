69769da961428e1d8bb68c7bfbb9ea9f9fa2598d
Brendan Whitfield <brendanw@windworksdesign.com>
2015-10-10 00:04:50 -0400

    select layout elements

--- js/layout.js
+++ js/layout.js
@@ -1,17 +1,33 @@
 
-
-/*
-	valid targets are:
-		home
-		project
-*/
+//The current layout
 var current = "home";
+
+var $content;
+var $header;
+var $name_projects;
+var $article;
+
+
+function init()
+{
+	$content = $("#content");
+	$header = $("header");
+	$name_projects = $("#name-projects");
+	$article = $("article");
+}
+
 
 /*
 	Function that moves to the requested layout.
 	If animate is false, the change will be instantanious.
-*/
-module.exports = function(target, animate)
-{
 
+	Valid targets are:
+		home
+		project
+*/
+module.exports = function(target, animate, callback)
+{
+	//if no arguments were passed, then this is an initialise call
+	if(arguments.length === 0)
+		return init();
 };
