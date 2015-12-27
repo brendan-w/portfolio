c662798141aac6e50d7bb2827074ecb4109fd7bb
Brendan Whitfield <brendanw@windworksdesign.com>
2015-12-26 22:56:24 -0500

    wrote release script to handle GH pages repo

---
+++ release.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "================================= building... =================================="
+
+# build the site
+npm run build
+
+PORTFOLIO=$PWD
+cd ../
+
+echo "=============================== checking git... ================================"
+
+# make sure the GitHub Pages repo is there
+# if not, close it
+[ -d "brendan-w.github.io/" ] || git clone https://github.com/brendan-w/brendan-w.github.io.git
+
+cd brendan-w.github.io/
+
+# blow away anything uncommitted, and pull any new changes
+git reset --hard
+git clean -fd
+git checkout master
+git pull origin master
+
+
+echo "============================== copying files... ================================"
+
+# update the files
+cp -rv $PORTFOLIO/site/* ./
+
+
+echo "=========================== regenerating symlinks... ==========================="
+
+# get a list of the page names (symlinks) to generate
+PAGES=$(ls $PORTFOLIO/html)
+
+# remove the old symlinks
+find ./ -type l -delete
+
+# generate new symlinks
+for PAGE in $PAGES
+do
+    ln -s index.html $PAGE
+    echo $PAGE
+done
+
+
+cd $PORTFOLIO
+
+echo "===================================== done ====================================="
